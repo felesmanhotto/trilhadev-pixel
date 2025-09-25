a4b2c9adf87474d76b5bbf0523499498a44f5cd5
felesmanhotto <146748681+felesmanhotto@users.noreply.github.com>
2025-09-25 11:58:47 -0300

    feat(navbar): adiciona elementos da navbar e estilo (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './styles/global.css'
 import './styles/theme.css'
+import { Navbar } from './components/Navbar'
 
 export function App() {
     return (
+        <>
+        <Navbar />
         <div>App inicial</div>
+        </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { Navbar } from './components/Navbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
