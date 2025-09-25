0c444275662bd8450d616c267d916471ac6121c8
felesmanhotto <felesmanhotto1@gmail.com>
2025-09-25 11:54:41 -0300

    feat(navbar): adiciona elementos da navbar e estilo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Leaf } from 'lucide-react';
+
+export function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="nav-group">
+                <Leaf color="#37AD3B" size={36}/>
+                <h3>EcoFuture</h3>
+            </div>
+            <div className="nav-group">
+                <p>Home</p>
+                <p>Sobre</p>
+                <p>Benefícios</p>
+                <p>Depoimentos</p>
+                <p>Contato</p>
+            </div>
+            <p className="button-small">Participe</p>
+        </div>
+    )
+}
