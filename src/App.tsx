18056789c442d56748e58d64dd6a952efebd4303
felesmanhotto <felesmanhotto1@gmail.com>
2025-09-26 10:51:07 -0300

    feat(hero): adiciona componente hero com ajustes em style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './styles/global.css'
 import './styles/theme.css'
 import { Navbar } from './components/Navbar'
-
+import { Hero } from './components/Hero'
 export function App() {
     return (
         <>
-        <Navbar />
-        <div>App inicial</div>
+            <Navbar />
+            <Hero />
         </>
     )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export function Hero() {
+    return (
+        <section className="hero">
+            <h1 className="hero-title">Building a Sustainable <span>Future</span> For All</h1>
+            <h4 className="hero-text">Join us in creating lasting change through renewable energy initiatives, environmental conservation, and community empowerment.</h4>
+            <div className="hero-buttons">
+                <p className="button">Donate Now</p>
+                <p className="button button-reverse">Get Involved</p>
+            </div>
+        </section>
+    )
+}
