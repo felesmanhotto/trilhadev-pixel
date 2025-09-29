5b466f138cdb5340003aa06be66731c820114755
felesmanhotto <146748681+felesmanhotto@users.noreply.github.com>
2025-09-29 10:12:14 -0300

    Feature/hero (#4)
    * feat(hero): adiciona componente hero com ajustes em style
    
    * feat(hero): ajusta fonte e tamanho do texto

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,12 @@
 export function Hero() {
     return (
         <section className="hero">
-            <h1 className="hero-title">Building a Sustainable <span>Future</span> For All</h1>
-            <h4 className="hero-text">Join us in creating lasting change through renewable energy initiatives, environmental conservation, and community empowerment.</h4>
+            <h1 className="hero-title">Construindo um Futuro <span className="text-green">Sustentável</span> Para Todos</h1>
+            <h4 className="hero-text">Junte-se a nós criando mudanças duradouras através de iniciativas de energias renováveis, preservação do meio
+                ambiente e empoderamento da comunidade.</h4>
             <div className="hero-buttons">
-                <p className="button">Donate Now</p>
-                <p className="button button-reverse">Get Involved</p>
+                <p className="button">Doe agora</p>
+                <p className="button button-reverse">Participe</p>
             </div>
         </section>
     )
